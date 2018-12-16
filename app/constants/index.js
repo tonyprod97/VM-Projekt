28e94c958354fa904dd3f55d6a270110a8361631
Kristijan Vrbanc <kv50209@fer.hr>
2018-12-16 19:09:40 +0100

    nadodani neki tagovi za dokumentaciju

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,12 +1,9 @@
 /**
- *
- * @type {number}
+ * @file U ovoj datoteci se izlazu kao moduli numericka varijabla "port", objekt "databaseErrors",
+ * objekt "databaseGetRequest" te objekt "databaseSendRequests" 
  */
 module.exports.port = 3000;
-/**
- *
- * @type {{OPERATION_SUCCESS: number, OPERATION_FAILED: number, OPERATION_WARRNING: number, OPERATION_DENIED: number}}
- */
+
 module.exports.databaseErrors = {
 
     OPERATION_SUCCESS : 0, // all good
@@ -14,19 +11,13 @@ module.exports.databaseErrors = {
     OPERATION_WARRNING: 2, // operation succeded but might couse problems
     OPERATION_DENIED  : 3  // operation might be dangoruse so it was blocked
 };
-/**
- *
- * @type {{USER: number}}
- */
+
 module.exports.databaseGetRequests = {
     USER             : 1, // request developers
     VISIBLE_CALENDARS: 2, // returns all users a current user has rights to see there calendar (needs in data field: userid, token)
     GET_VERIFICATION : 4  //request verification details about user (needs in data field: email) returns (verified, userid *, token *) * -> if not verified 
 };
-/**
- *
- * @type {{CREATE_NEW_USER: number, LOGIN_REQUEST: number, TERMINATE_SESSION: number}}
- */
+
 module.exports.databaseSendRequests = {
 
     CREATE_NEW_USER  : 1, // create new user  (needs in data field: username, email, password) 
