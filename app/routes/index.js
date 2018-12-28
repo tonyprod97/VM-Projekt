8880c56f9d456da761217103ea588daac3ee447b
Lovro Knežević <lovro.knezevic1@gmail.com>
2018-12-28 11:58:25 +0100

    Added outlook event get in new branch d1.

--- app/routes/index.js
+++ app/routes/index.js
@@ -14,6 +14,8 @@ const ids             = require('../constants').databaseGetRequests;
 const operationStates = require('../constants').databaseErrors; 
 
 const authHelper = require('../OutlookManager');
+var outlook = require('node-outlook');
+var moment = require('moment');
 const session = require('express-session');
 
 router.use(session(
@@ -206,6 +208,102 @@ router.get('/logout', function (req, res) {
 
 router.get('/home', (req,res)=>{
     res.render('home');
+});
+
+/*
+    JSONPathovi.
+    za naslov - $.Subject
+    početak eventa - $.Start.DateTime
+    kraj eventa - $.End.DateTime
+    mjesto događaja - $.Location.DisplayName
+
+ */
+
+router.get('/sync', function(req, res) {
+    var token = req.session.access_token;
+    var email = req.session.email;
+    if (token === undefined || email === undefined) {
+        console.log('/sync called while not logged in');
+        res.redirect('/');
+        return;
+    }
+
+    // Set the endpoint to API v2
+    outlook.base.setApiEndpoint('https://outlook.office.com/api/v2.0');
+    // Set the user's email as the anchor mailbox
+    outlook.base.setAnchorMailbox(req.session.email);
+    // Set the preferred time zone
+    outlook.base.setPreferredTimeZone('Eastern Standard Time');
+
+    // Use the syncUrl if available
+    var requestUrl = req.session.syncUrl;
+    if (requestUrl === undefined) {
+        // Calendar sync works on the CalendarView endpoint
+        requestUrl = outlook.base.apiEndpoint() + '/Me/CalendarView';
+    }
+
+    // Set up our sync window from midnight on the current day to
+    // midnight 7 days from now.
+    var startDate = moment().startOf('day');
+    var endDate = moment(startDate).add(30, 'days');
+    // The start and end date are passed as query parameters
+    var params = {
+        startDateTime: startDate.toISOString(),
+        endDateTime: endDate.toISOString()
+    };
+
+    // Set the required headers for sync
+    var headers = {
+        Prefer: [
+            // Enables sync functionality
+            'odata.track-changes',
+            // Requests only 5 changes per response
+            'odata.maxpagesize=5'
+        ]
+    };
+
+    var apiOptions = {
+        url: requestUrl,
+        token: token,
+        headers: headers,
+        query: params
+    };
+
+    console.log('requestUrl ' + apiOptions.url);
+    console.log('token ' + apiOptions.token);
+    console.log('headers ' + apiOptions.headers);
+    console.log('params ' + apiOptions.query);
+
+    outlook.base.makeApiCall(apiOptions, function(error, response) {
+        if (error) {
+            console.log(JSON.stringify(error));
+            res.send(JSON.stringify(error));
+        }
+        else {
+            if (response.statusCode !== 200) {
+                console.log('API Call returned ' + response.statusCode);
+                res.send('API Call returned ' + response.statusCode);
+            }
+            else {
+                console.log('API Call returned ' + response.statusCode);
+                var nextLink = response.body['@odata.nextLink'];
+                if (nextLink !== undefined) {
+                    req.session.syncUrl = nextLink;
+                }
+                var deltaLink = response.body['@odata.deltaLink'];
+                if (deltaLink !== undefined) {
+                    req.session.syncUrl = deltaLink;
+                }
+
+                //var finalResponse=JSON.stringify(response.body.value);
+                //console.log(JSON.stringify(response.body.value))
+                console.log('Final respone: '+ JSON.stringify(response.body.value));
+                //res.render('/',{finalResponse});
+                res.redirect('/');
+
+            }
+        }
+    });
 });
 
 module.exports = router;
